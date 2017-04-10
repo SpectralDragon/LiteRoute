0db68530b3f0210904f682c132ca5869135d15dd
Владислав Прусаков <spectraldragonchannel@gmail.com>
2017-04-10 21:32:24 +0300

    Initial Commit

---
+++ LightRoute/LightRoute.h
@@ -0,0 +1,19 @@
+//
+//  LightRoute.h
+//  LightRoute
+//
+//  Created by Владислав Прусаков on 10/04/2017.
+//  Copyright © 2017 WebAnt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LightRoute.
+FOUNDATION_EXPORT double LightRouteVersionNumber;
+
+//! Project version string for LightRoute.
+FOUNDATION_EXPORT const unsigned char LightRouteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LightRoute/PublicHeader.h>
+
+
