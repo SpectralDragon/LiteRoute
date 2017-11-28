2f1bee6f99447c34c4866b778af8454058336c75
Andrey Krivobok <krivobok@azoft.com>
2017-11-28 12:58:08 +0700

    ability to perform segue synchronously

--- LightRoute/TransitionNodes/SegueTransitionNode.swift
+++ LightRoute/TransitionNodes/SegueTransitionNode.swift
@@ -42,50 +42,69 @@ public final class SegueTransitionNode<T>: GenericTransitionNode<T> {
 		self.transitioningDelegate = transitioningDelegate
 		return self
 	}
-	
+
     ///
+    /// This method is responsible for the delivery of the controller for the subsequent initialization, then there is a transition.
+    ///
+    /// - Parameter async: If true, then segue performs async.
+    /// - Parameter block: Initialize controller for transition and fire.
+    /// - Throws: Throw error, if destination was nil or could not be cast to type.
+    ///
+	public func then(async: Bool, _ block: @escaping TransitionSetupBlock<T>) throws {
+
+        let thenBody = {
+            self.root.performSegue(withIdentifier: self.segueIdentifier, sender: nil, completion: { segue in
+                var destination = segue.destination
+                destination.transitioningDelegate = self.transitioningDelegate
+
+                if destination is UINavigationController {
+                    destination = (segue.destination as! UINavigationController).topViewController ?? segue.destination
+                } else if destination is UITabBarController {
+                    let tabBarController = (segue.destination as! UITabBarController)
+                    guard let viewControllers = tabBarController.viewControllers else {
+                        throw LightRouteError.customError("ViewControllers in UITabBarController can't be nil")
+                    }
+
+                    for controller in viewControllers {
+                        let moduleInput = (self.customModuleInput != nil) ? self.customModuleInput : destination.moduleInput
+                        if moduleInput is T || controller is T {
+                            destination = controller
+                            break
+                        }
+                    }
+                }
+
+                var output: Any?
+
+                let destinationModuleInput = (self.customModuleInput != nil) ? self.customModuleInput : destination.moduleInput
+                if let moduleInput = destinationModuleInput, moduleInput is T {
+                    output = block(destination.moduleInput as! T)
+                } else if destination is T {
+                    output = block(destination as! T)
+                } else {
+                    throw LightRouteError.castError(controller: String(describing: destination.self), type: "\(self.type)")
+                }
+
+                segue.source.moduleOutput = output
+            })
+        }
+
+        if async {
+            DispatchQueue.main.async {
+                thenBody()
+            }
+        } else {
+            thenBody()
+        }
+	}
+
+	///
     /// This method is responsible for the delivery of the controller for the subsequent initialization, then there is a transition.
     ///
     /// - Parameter block: Initialize controller for transition and fire.
     /// - Throws: Throw error, if destination was nil or could not be cast to type.
     ///
 	public override func then(_ block: @escaping TransitionSetupBlock<T>) throws {
-		DispatchQueue.main.async {
-			self.root.performSegue(withIdentifier: self.segueIdentifier, sender: nil, completion: { segue in
-				var destination = segue.destination
-				destination.transitioningDelegate = self.transitioningDelegate
-				
-				if destination is UINavigationController {
-					destination = (segue.destination as! UINavigationController).topViewController ?? segue.destination
-				} else if destination is UITabBarController {
-					let tabBarController = (segue.destination as! UITabBarController)
-					guard let viewControllers = tabBarController.viewControllers else {
-						throw LightRouteError.customError("ViewControllers in UITabBarController can't be nil")
-					}
-					
-					for controller in viewControllers {
-						let moduleInput = (self.customModuleInput != nil) ? self.customModuleInput : destination.moduleInput
-						if moduleInput is T || controller is T {
-							destination = controller
-							break
-						}
-					}
-				}
-				
-				var output: Any?
-				
-				let destinationModuleInput = (self.customModuleInput != nil) ? self.customModuleInput : destination.moduleInput
-				if let moduleInput = destinationModuleInput, moduleInput is T {
-					output = block(destination.moduleInput as! T)
-				} else if destination is T {
-					output = block(destination as! T)
-				} else {
-					throw LightRouteError.castError(controller: String(describing: destination.self), type: "\(self.type)")
-				}
-				
-				segue.source.moduleOutput = output
-			})
-		}
-	}
-	
+        try then(async: true, block)
+    }
 }
